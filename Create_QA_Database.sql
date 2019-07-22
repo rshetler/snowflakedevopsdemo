1c4a7af0de70deabec9e0d81db7a6e751bedf12b
Rodney Shetler <rodney.shetler@live.com>
2019-07-22 08:04:11 -0400

    update QA script

--- Create_QA_Database.sql
+++ Create_QA_Database.sql
@@ -1,5 +1,5 @@
 --Clone the original prod database for QA purposes
-create database devops_demodb_QA clone devops_demodb;
+create database devops_demodb_QA clone devops_demodb_DEVELOPMENT;
 
 --Swtich to new QA database 
 use devops_demodb_QA.new_dataset;
@@ -7,9 +7,5 @@ use devops_demodb_QA.new_dataset;
 --********Begin QA Code Promotion Section ************
 --Ensure new tables are represented here to be cloned from Dev during promotion 
 
---1. Clone "customer" table in dev to promote to QA
-create or replace table customer clone devops_demodb_development.new_dataset.customer;
 
---2. Clone "nation" table in dev complete with any changes commited previously
-create or replace table nation clone devops_demodb_development.new_dataset.nation;
 
